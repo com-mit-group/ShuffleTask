5aab95b0485dce0ff6217a4e620449795d91884e
yaron-E92 <yaronefrat92@gmail.com>
2025-09-25 13:57:53 +0200

    [#19]deduplication: Refactor Apple app delegate coordination

---
+++ Platforms/Apple/ShuffleCoordinatorApplicationDelegate.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.DependencyInjection;
+using ShuffleTask.Services;
+using UIKit;
+
+namespace ShuffleTask;
+
+internal abstract class ShuffleCoordinatorApplicationDelegate : MauiUIApplicationDelegate
+{
+    public override void OnActivated(UIApplication uiApplication)
+    {
+        base.OnActivated(uiApplication);
+
+        ResumeCoordinator();
+    }
+
+    public override void DidEnterBackground(UIApplication uiApplication)
+    {
+        PauseCoordinator();
+
+        base.DidEnterBackground(uiApplication);
+    }
+
+    private static void ResumeCoordinator()
+    {
+        var coordinator = MauiProgram.TryGetServiceProvider()?.GetService<ShuffleCoordinatorService>();
+        if (coordinator != null)
+        {
+            _ = coordinator.ResumeAsync();
+        }
+    }
+
+    private static void PauseCoordinator()
+    {
+        var coordinator = MauiProgram.TryGetServiceProvider()?.GetService<ShuffleCoordinatorService>();
+        if (coordinator != null)
+        {
+            _ = coordinator.PauseAsync();
+        }
+    }
+}
--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -1,34 +1,9 @@
 using Foundation;
-using Microsoft.Extensions.DependencyInjection;
-using ShuffleTask.Services;
-using UIKit;
 
 namespace ShuffleTask;
 
 [Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+public class AppDelegate : ShuffleCoordinatorApplicationDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-
-    public override void OnActivated(UIApplication uiApplication)
-    {
-        base.OnActivated(uiApplication);
-
-        var coordinator = MauiProgram.TryGetServiceProvider()?.GetService<ShuffleCoordinatorService>();
-        if (coordinator != null)
-        {
-            _ = coordinator.ResumeAsync();
-        }
-    }
-
-    public override void DidEnterBackground(UIApplication uiApplication)
-    {
-        var coordinator = MauiProgram.TryGetServiceProvider()?.GetService<ShuffleCoordinatorService>();
-        if (coordinator != null)
-        {
-            _ = coordinator.PauseAsync();
-        }
-
-        base.DidEnterBackground(uiApplication);
-    }
 }
